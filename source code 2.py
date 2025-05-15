fa8a6e7117fb9fcd16aefde566d8f12dfedbcbbe
sanjana-project-work <sanjanasenthilss@gmail.com>
2025-05-16 03:43:09 +1030

    Add files via upload

--- source code 2.py
+++ source code 2.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
+import plotly.express as px
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import LabelEncoder, StandardScaler
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import classification_report, confusion_matrix
+
 st.title("Customer Churn Prediction App")
 
 # File upload
